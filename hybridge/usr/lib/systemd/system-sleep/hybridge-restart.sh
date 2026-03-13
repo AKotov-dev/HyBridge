4815b936791fd4d369f454516e2357586f235707
AKotov-dev <akotov830@gmail.com>
2026-03-13 21:23:43 +0300

    v0.3 + Restart after sleep, suspend, hibernate

--- hybridge/usr/lib/systemd/system-sleep/hybridge-restart.sh
+++ hybridge/usr/lib/systemd/system-sleep/hybridge-restart.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+# Этот hook вызывается дважды: pre и post
 [ "$1" != "post" ] && exit 0
 
+# Небольшая задержка, чтобы user dbus успел подняться
 sleep 1
 
-loginctl list-users --no-legend | awk '{print $1}' | while read -r uid; do
-    user_name=$(id -nu "$uid")
+# Перебираем все активные user runtimes
+for uid_dir in /run/user/*; do
+    uid=${uid_dir##*/}
 
+    # Пропускаем не числовые каталоги
+    [[ "$uid" =~ ^[0-9]+$ ]] || continue
+
+    user=$(id -nu "$uid")
+
+    # Проверяем, что systemd user manager запущен
     if [ -d "/run/user/$uid" ]; then
-        runuser -u "$user_name" -- \
-            env XDG_RUNTIME_DIR="/run/user/$uid" \
-            systemctl --user is-enabled --quiet hybridge.service && \
-        runuser -u "$user_name" -- \
-            env XDG_RUNTIME_DIR="/run/user/$uid" \
+        runuser -u "$user" -- env XDG_RUNTIME_DIR="/run/user/$uid" bash -c '
+            systemctl --user is-enabled --quiet hybridge.service &&
             systemctl --user restart hybridge.service
+        '
     fi
 done
